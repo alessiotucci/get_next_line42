4579c444c94cd39ac7f120fdb928f6850db792c6
Alessio Tucci <atucci@e3r4p7.42roma.it>
2023-02-03 15:32:57 +0100

    adding a lot

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 10:39:29 by atucci            #+#    #+#             */
-/*   Updated: 2023/02/03 14:37:30 by atucci           ###   ########.fr       */
+/*   Updated: 2023/02/03 15:10:58 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,15 @@ int main()
 {
 	int fd;
 	int	len;
-	char *pointer; // this  pointer  is used in Read function
+	 char *pointer; // this  pointer  is used in Read function
 	char *final;	// this pointer is used to Printf function
 	int count;
-	
 	
 	pointer = malloc(sizeof(char) * 100);
 	if (pointer == NULL)
 		return (0);
+
+	printf( "pointer address: %p\n", pointer);
 	// checking the dynamic allocation of memory
 	fd = open("shortsubject.txt", O_RDONLY);
 	int fd1 = open("testo.txt", O_RDONLY);
@@ -41,42 +42,49 @@ int main()
 	printf( "this is  the file  descriptor: %d\n", fd1);
 
 	// read function allow me to read the text file
-	read(fd,pointer,50);
+	int	bytes_read = read(fd,pointer,50);
 	// it store the information in the pointer passed as paramater
 	len = strlen(pointer);
 	
-	// allocating the memory for final, which will contain the line with the desired char
-	final = malloc(sizeof(char) * len + 1);
-	if (final == NULL)
-		return (0);
-
-	count = 0;
-	while (pointer[count] != '\n')
+	// loops throught the entire text file
+	while (bytes_read != 0)
 	{
-		final[count] = pointer[count];
-		count++;
-		// if the end of pointer is reached, allocate more memory and read 
-		// the next chuck of memory
-			if (count == len)
-			{
-				len += 100;
-				pointer = realloc(pointer, sizeof(char) * (len + 1));
-				if (pointer == NULL)
-					return (0);
-			read (fd, pointer + count, 100);
-			}
-	}
-
-	// add null terminator to the final
-	final[count] = '\0';
+		// allocating the memory for final, which will contain the line with the desired char
+		final = malloc(sizeof(char) * len + 1);
+		if (final == NULL)
+			return (0);
+		printf( "final address: %p\n", final);
+		count = 0;
+		while (pointer[count] != '\n')
+		{
+			final[count] = pointer[count];
+			count++;
+			// if the end of pointer is reached, allocate more memory and read 
+			// the next chuck of memory
+				if (count == len)
+				{
+					len += 100;
+					pointer = realloc(pointer, sizeof(char) * (len + 1));
+					if (pointer == NULL)
+						return (0);
+				read (fd, pointer + count, 100);
+				}
+		}
 	
-	// print the result
-	printf( "this is the line: %s", pointer);
-	printf( "lenght: %d\n", len);
-	printf( "%s", final);
-	// freeing and closing the  file
-	close(fd);
+		// add null terminator to the final
+		final[count] = '\0';
+		
+		// print the result
+		printf( "this is the line: %s", pointer);
+		printf( "lenght: %d\n", len);
+		printf( "%s", final);
+		// freeing and closing the  file
+		free(final);
+		bytes_read = read(fd, pointer, 100);
+		len = strlen(pointer);
+	}
+		close(fd);
 	free(pointer);
-	//close(fd);
+	return (0);	
 }
 
